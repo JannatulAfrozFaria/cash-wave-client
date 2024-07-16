e9725bb1b512e0d843761a2a050d0ab03aa883ee
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-07-17 04:14:04 +0600

    icons modified in dashboard

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,6 +1,9 @@
 import { FaBars, FaBook, FaCalendar, FaHome, FaUsers } from "react-icons/fa";
 import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
-import { MdAddCard } from "react-icons/md";
+import { BsCashCoin } from "react-icons/bs";
+import { GiTakeMyMoney } from "react-icons/gi";
+
+import { HiOutlineDocumentCheck } from "react-icons/hi2";
 import { IoPersonAddSharp } from "react-icons/io5";
 
 import { ImProfile } from "react-icons/im";
@@ -32,10 +35,6 @@ const Dashboard = () => {
     }
     return (
         <div className="flex">
-        {/* Dashboard Content */}
-        <div className="flex-1">
-           <Outlet></Outlet>
-       </div>
        {/* Dashboard Side Bar */}
        <div className="w-40 md:w-64 min-h-screen bg-blue-200">
            <ul className="menu p-4 uppercase">
@@ -46,13 +45,13 @@ const Dashboard = () => {
                                <img className='w-1/2 mx-auto my-2' src="/src/assets/icons/icon.png" alt="" />
                            </li>
                            <li>
-                               <NavLink to="/dashboard/AddAnAsset"><MdAddCard className="text-lg mr-0 md:mr-2" /> Add an Asset</NavLink>
+                               <NavLink to="/dashboard/AddAnAsset"><BsCashCoin className="text-lg mr-0 md:mr-2" /> Cash In</NavLink>
                            </li>
                            <li>
-                               <NavLink to="/dashboard/myEmployeeList"><FaUsers className="text-lg mr-0 md:mr-2"></FaUsers>My Employee list</NavLink>
+                               <NavLink to="/dashboard/myEmployeeList"><GiTakeMyMoney className="text-lg mr-0 md:mr-2"/>Cash Out</NavLink>
                            </li>
                            <li>
-                               <NavLink to="/dashboard/addAnEmployee"> <IoPersonAddSharp className="text-lg mr-0 md:mr-2" /> Add an Employee </NavLink>
+                               <NavLink to="/dashboard/addAnEmployee"> <HiOutlineDocumentCheck className="text-lg mr-0 md:mr-2" /> Register </NavLink>
                            </li>
                        </> 
                    :
@@ -79,7 +78,7 @@ const Dashboard = () => {
                <li>
                     {/* THEME------CHANGE------BUTTON */}
                     <label className="cursor-pointer grid place-items-center mr-2 md:mr-8">
-                        <input onChange={handleTheme} type="checkbox" className="toggle theme-controller bg-green-700 row-start-1 col-start-1 col-span-2"/>
+                        <input onChange={handleTheme} type="checkbox" className="toggle theme-controller bg-blue-500 row-start-1 col-start-1 col-span-2"/>
                         <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
                         <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
                     </label>
@@ -104,6 +103,10 @@ const Dashboard = () => {
                    </>} */}
                </li>
            </ul>
+       </div>
+       {/* Dashboard Content */}
+       <div className="flex-1">
+           <Outlet></Outlet>
        </div>
    </div>
     );
