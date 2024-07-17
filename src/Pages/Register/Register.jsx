4f447a36fa679f5ce237aafcc3375032ae1a5dee
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-07-17 17:59:57 +0600

    registration form created

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -16,23 +16,36 @@ const Register = () => {
                     </div>
                     <div className="card bg-base-100 w-2/3 shadow-2xl">
                         <form className="card-body">
+                            {/* NAME--------- */}
                             <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="email" placeholder="email" className="input input-bordered" required />
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered" required />
                             </div>
+                            {/* EMAIL______ */}
                             <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="password" placeholder="password" className="input input-bordered" required />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            {/* MOBILE---NUMBER */}
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Mobile Number</span>
+                                </label>
+                                <input type="number" placeholder="Mobile Number" className="input input-bordered" required />
+                            </div>
+                            {/* PIN----NUMBER */}
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">5-digit PIN</span>
+                                </label>
+                                <input type="password" placeholder="PIN Number" className="input input-bordered" required />
                             </div>
                             <div className="form-control mt-6">
-                            <button className="btn btn-base">Login</button>
+                            <button className="btn btn-base">Register</button>
                             </div>
                         </form>
                     </div>
